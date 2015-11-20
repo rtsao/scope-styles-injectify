04f2063d8d44fa3edece0761d7fd4c01d2fea2f2
Ryan Tsao <rtsao@uber.com>
2015-11-19 18:19:21 -0800

    Test buildtime transform

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var through = require('through2');
 var requireFromString = require('require-from-string');
+var cssKey = require('scope-styles/lib/css-symbol');
 
 module.exports = function (filename, opts) {
   var extension = '.style.js';
@@ -14,7 +15,7 @@ module.exports = function (filename, opts) {
   var output = through(function(buf, enc, next) {
     var source = buf.toString('utf8');
     var transformer = runtime ? transformRuntime : transformBuildtime;
-    
+
     try {
       var transformedSource = transformer(source, filename);
     } catch (err) {
@@ -33,24 +34,71 @@ module.exports = function (filename, opts) {
   return output;
 };
 
-function transformRuntime(source, filename) {
-  var styles = requireFromString(source, filename);
-  var result = scopeStyles(styles);
-  return [
-    'var cssKey = require(\'scope-styles/lib/css-symbol\');',
-    'var result = ' + JSON.stringify(result) + '};',
-    'result[cssKey] = \'' + scopeStyles.getCss(result) + '\';',
-    'module.exports = result;'
-  ].join('\n');
-}
-
-function transformBuildtime(source, filename) {
-  return 'module.exports = {lol: 1337};'
-}
-
 function matchesExtension(filename, extension) {
   var start = filename.length - extension.length;
   var end = filename.length;
   return filename.substring(start, end) === extension;
 }
 
+function walkAst(source, walkFn) {
+  return falafel(source, {
+    parser: acorn,
+    ecmaVersion: 6,
+    sourceType: 'module'
+  }, walkFn);
+}
+
+/*
+ * Runtime Transformation
+ */
+
+function transformRuntime(source, filename) {
+  return walkAst(source, swapScopeStyles);
+}
+
+var regex = /(['"`])scope-styles\1/;
+
+function swapScopeStyles(node) {
+  if (node.type === 'ImportDeclaration') {
+    node.update(node.source().replace(regex,
+      '$1scope-styles-injectify/scope-styles-inject$1'));
+  } else if (isRequireScopeStyles(node)) {
+    var quote = node.arguments[0].raw[0][0];
+    var str = quote + 'scope-styles-injectify/scope-styles-inject' + quote;
+    node.arguments[0].update(str);
+  }
+}
+
+/*
+ * Buildtime Transformation
+ */
+
+function transformBuildtime(source, filename) {
+  // var instrumentedModule = walkAst(source, instrumentedModule);
+  var instrumentedModule = source + ';module.exports[require("scope-styles/lib/css-symbol")] = "stuff";'
+  var css = requireFromString(instrumentedModule)[cssKey];
+  return source + ';require("insert-css")(' + css + ');';
+}
+
+function instrumentModule(node) {
+  if (node.type === 'ImportDeclaration') {
+    // handle possible scope styles action
+  } else if (isRequireScopeStyles(node)) {
+    if (node.arguments[0].value === 'scope-styles') {
+      // handle stuff
+    }
+  }
+}
+
+/*
+ * General AST Helpers
+ */
+
+function isRequireScopeStyles(node) {
+  return node.callee &&
+    node.type === 'CallExpression' &&
+    node.callee.type === 'Identifier' &&
+    node.callee.name === 'require' &&
+    node.arguments[0] &&
+    node.arguments[0].value === 'scope-styles';
+}
